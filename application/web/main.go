5f2d7564121482584afbe1bcdae8b5267679a402
Irsyad Rizaldi <irsyad.rizaldi@bukalapak.com>
2020-12-13 20:23:25 +0700

    Add .env file

--- application/web/main.go
+++ application/web/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/julienschmidt/httprouter"
 	"go-boilerplate/component"
+	"log"
 	"net/http"
 )
 
 func main() {
+	// load environment
+	err := godotenv.Load()
+	warnOnError(err)
+
 	// build component
 	postgresDB, err := component.NewPostgresDB()
 	panicOnError(err)
@@ -25,6 +31,12 @@ func main() {
 	// run server
 	err = server.ListenAndServe()
 	panicOnError(err)
+}
+
+func warnOnError(err error)  {
+	if err != nil {
+		log.Print(err)
+	}
 }
 
 func panicOnError(err error) {
