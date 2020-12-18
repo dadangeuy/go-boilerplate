3afa64ff467d3c949eb7e17bdec343d1d8a83f95
Irsyad Rizaldi <irsyad.rizaldi@bukalapak.com>
2020-12-18 21:08:01 +0700

    Implement list user (delivery)

---
+++ domain/user/delivery/list.go
@@ -0,0 +1,21 @@
+package delivery
+
+import (
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
+
+func (d *Delivery) ListHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	users, err := d.Usecase.FindAll()
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	err = json.NewEncoder(w).Encode(users)
+	if err != nil {
+		panic(err)
+	}
+}
